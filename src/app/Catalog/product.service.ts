65e32bdb2ba29f5e84797f7eb2e976de8533ec5f
Rohit Ingawale <RohitI@nedbank.co.za>
2025-11-12 17:15:40 +0530

    Changes

--- src/app/Catalog/product.service.ts
+++ src/app/Catalog/product.service.ts
@@ -1,44 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Product } from './models/product';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class ProductService {
-
-  private products: Product[] = [
+   list:any=[
       {"id": 12,  "title":"Lotus", "description": "Wedding flower","price":24 , "likes":800, "imageurl":"/assets/images/lotus.jpg"},
       {"id": 13,  "title":"Rose", "description": "Valentine flower","price":14, "likes":4000, "imageurl":"/assets/images/rose.jpg"},
       {"id": 14,  "title":"Jasmine", "description": "Smelling flower","price":3, "likes":9000, "imageurl":"/assets/images/jasmine.jpg"},
       {"id": 15,  "title":"Tulip", "description": "Beautiful flower","price":16, "likes":3000, "imageurl":"/assets/images/tulip.jpg"},
       {"id": 16,  "title":"Lily", "description": "Delicate flower","price":9,"likes":6000, "imageurl":"/assets/images/lily.jpg"},
-      {"id": 17,  "title":"Marigold", "description": "Festival flower","price":4,"likes":56000, "imageurl":"/assets/images/marigold.jpg"},
-  ]
+      {"id": 17,  "title":"Marigold", "description": "Festival flower","price":4,"likes":56000, "imageurl":"/assets/images/marigold.jpg"}
+  ];
 
   constructor() { }
- 
-  getAll():  Product[]{
-    return this.products;
-  }
 
+  getAllProducts():any
+  { return this.list; }
   
-  getById(id: number):Product {
-    return this.products.find(p => p.id === id) as Product;
-  }
-
-  add(product: Product): Product {
-    product.id = this.products.length + 1;
-    this.products.push(product);
-    return product;
-  }
-
-  update(product: Product): Product {
-    const index = this.products.findIndex(p => p.id === product.id);
-    if (index !== -1) {
-      this.products[index] = product;
-    }
-    return product;
+  getProductById(id:number):any
+  { 
+    console.log("selected product id="+ id);
+    //return {id:6,title:'Lily',description:"wedding Flower",unitPrice:10, quantity:2300,likes:76,imageUrl:"./assets/images/gerbera.jpg"};
+   return this.list.find((p:any)=>{ return p.id ==id});
   }
   
-  delete(id: number): void {
-    this.products = this.products.filter(p => p.id !== id);
+  updateProduct(prod:any):void
+  {
+    let index=this.list.findIndex((p:any)=> p.id==prod.id);
+    if(index != -1)
+     this.list[index]=prod;
   }
-  } 
+}
